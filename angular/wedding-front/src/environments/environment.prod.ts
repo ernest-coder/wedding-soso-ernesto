dd3ba40172628031bd28a68482cb76d6db5a46a7
echoppindejanvry <echoppindejanvry@deloitte.fr>
2025-09-10 21:53:49 +0200

    build vercel

--- angular/wedding-front/src/environments/environment.prod.ts
+++ angular/wedding-front/src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: false,
+  supabaseUrl: 'https://fycdhuyfufwxkiyoewis.supabase.co',
+  supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ5Y2RodXlmdWZ3eGtpeW9ld2lzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTY5MTU3MTQsImV4cCI6MjA3MjQ5MTcxNH0.wN7oCroTZ4FVxP7BpNWmtDHjdWOBqYUsBoPDToyE3Fo'
 };
